94182c56c7eea745d4ea033695421a3cf0bc5b9b
drive2yon <yon2yon2yon@gmail.com>
2017-06-24 12:43:35 +0900

    Load credentials file. gitignore test for credentials file.

--- MyBookReading/MyBookReadingPage.xaml.cs
+++ MyBookReading/MyBookReadingPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
 
 namespace MyBookReading
 {
@@ -19,6 +22,19 @@ namespace MyBookReading
             myBookListPage.Title = "MyBook";
             bookSearchPage.Title = "BookSearch";
             amazonPage.Title = "AmazonSearch";
+
+            LoadCredentialsFile();
+        }
+
+        private void LoadCredentialsFile()
+        {
+            var assembly = typeof(MyBookReadingPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("MyBookReading.Assets.TestCredentials.json");
+			string text = "";
+			using (var reader = new System.IO.StreamReader(stream))
+			{
+				text = reader.ReadToEnd();
+			}
         }
     }
 }
