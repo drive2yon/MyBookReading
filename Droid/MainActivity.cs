6834e76cdf91c4324df336d5b43a6712654a815f
drive2yon <yon2yon2yon@gmail.com>
2017-09-08 07:54:20 +0900

    Add Splash Screen.

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace MyBookReading.Droid
 {
-    [Activity(Label = "MyBookReading.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MyBookReading.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme"/*, MainLauncher = true*/, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
