b5a61643016b1369b4098ae1cb0c88b8ed144159
drive2yon <yon2yon2yon@gmail.com>
2018-05-27 21:04:11 +0900

    - Add GoogleAnalytics - Update Android Manifest Version

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.GoogleAnalytics;
 
 namespace MyBookReading.Droid
 {
@@ -19,6 +20,12 @@ namespace MyBookReading.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+
+            GoogleAnalytics.Current.Config.TrackingId = "UA-119889066-1";
+            GoogleAnalytics.Current.Config.AppId = this.PackageManager.GetPackageInfo(this.PackageName, 0).PackageName;
+            GoogleAnalytics.Current.Config.AppName = "図書館ほんだな";
+            GoogleAnalytics.Current.Config.AppVersion = this.PackageManager.GetPackageInfo(this.PackageName, 0).VersionName;
+            GoogleAnalytics.Current.InitTracker();
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
